e0859e9f30b61666112457aaf0f961d811a977c1
kimminju <blesty5683@naver.com>
2022-03-21 23:19:17 +0900

    map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
       <h3>map</h3>
+      <div id="map" style="width:500px;height:400px;"></div>
   </div>
 </template>
 
 <script>
 export default {
     setup (){
-
+        
 
         return{}
     }
